b26f8e417574ccaf8f46354f8597a839999db763
zachdicklin <zdicklin@gmail.com>
2024-03-15 15:16:02 -0600

    Improve presentation

--- query.py
+++ query.py
@@ -1,4 +1,7 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, StorageContext, load_index_from_storage
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, StorageContext, load_index_from_storage, get_response_synthesizer
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.postprocessor import SimilarityPostprocessor
 from time import sleep
 import os
 
@@ -19,7 +22,19 @@ else:
     service_context = ServiceContext.from_defaults(chunk_size=512)
     index = VectorStoreIndex.from_documents(documents, service_context=service_context, show_progress=True)
     index.storage_context.persist(persist_dir="./storage")
-query_engine = index.as_query_engine()
+
+retriever = VectorIndexRetriever(
+    index=index,
+    similarity_top_k=10,
+)
+response_synthesizer = get_response_synthesizer()
+# query_engine = index.as_query_engine()
+
+query_engine = RetrieverQueryEngine(
+    retriever=retriever,
+    response_synthesizer=response_synthesizer,
+    node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
+)
 
 
 history = []
@@ -38,16 +53,20 @@ while True:
         merged_history += f"Query: {q}\nResponse: {r}\n\n"
     merged_history += "\n\n----\n\n" if len(history) > 0 else ""
 
-    full_query = merged_history + "Query: " + query
+    full_query = merged_history + "Query: " + query + " (justify your response with the context)"
     response = query_engine.query(full_query)
-    print("\n\n")
-    print(response)
-
+    
+    print("**********")
     print("Passages:")
     for source_node in response.source_nodes:
         # code_section = response.source_nodes[0].metadata['file_name'].split("_")[1].split(".")[0]
         print(source_node.text)
         print("\n")
+    print("**********")
 
+    print("Query: " + query)
+    print("\n\n")
+    print("Response: " + response.response)
+    print("\n\n")
 
     history = maintain_history(history, query, response.response, max_history)
